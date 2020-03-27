20dacbe82950b5900de174758e7e59cee53e536f
unlsycn <g.humphrey.sln@gmail.com>
2020-03-27 16:20:00 +0800

    Update

---
+++ MisakaTranslator-WPF/ISettings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using Config.Net;
+using HandyControl.Controls;
+
+namespace MisakaTranslator_WPF
+{
+    public interface ISettings
+    {
+        #region 界面设置
+        #region 前景色设置
+        [Option(Alias = "Appearance.Foreground", DefaultValue = "#F06292")]
+        string ForegroundHex { get; set; }
+        #endregion
+        #endregion
+        //API设置
+        //翻译设置
+    }
+}
--- MisakaTranslator-WPF/MainWindow.xaml.cs
+++ MisakaTranslator-WPF/MainWindow.xaml.cs
@@ -1,28 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using Config.Net;
+using HandyControl.Controls;
+using System.Drawing;
+using System.Runtime.CompilerServices;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace MisakaTranslator_WPF
 {
-    /// <summary>
-    /// MainWindow.xaml 的交互逻辑
-    /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+            ISettings settings = new ConfigurationBuilder<ISettings>().UseJsonFile("settings/settings.json").Build();
+            this.Resources["Foreground"] = (SolidColorBrush)(new BrushConverter().ConvertFrom(settings.ForegroundHex));
+        }
+
+        private static void InitializeApperence()
+        {
+            
         }
     }
 }
