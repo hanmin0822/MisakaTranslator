66f37003f5ae9a12e3b0456fc651722d18950d62
tpxxn <351765204@qq.com>
2020-04-06 21:52:12 +0800

    Canvas->StackPanel+Grid自适应布局

---
+++ MisakaTranslator-WPF/SettingsPages/TranslatorPages/TranslatorGeneralSettingsPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TranslatorLibrary;
+
+namespace MisakaTranslator_WPF.SettingsPages
+{
+    /// <summary>
+    /// TranslatorGeneralSettingsPage.xaml 的交互逻辑
+    /// </summary>
+    public partial class TranslatorGeneralSettingsPage : Page
+    {
+        private List<string> TranslatorList;
+            
+        public TranslatorGeneralSettingsPage()
+        {
+            InitializeComponent();
+            TranslatorList = CommonFunction.GetTranslatorList();
+            FirstTransCombox.ItemsSource = TranslatorList;
+            SecondTransCombox.ItemsSource = TranslatorList;
+
+            FirstTransCombox.SelectedIndex = CommonFunction.GetTranslatorIndex(Common.appSettings.FirstTranslator);
+            SecondTransCombox.SelectedIndex = CommonFunction.GetTranslatorIndex(Common.appSettings.SecondTranslator);
+
+            EachRowTransCheckBox.IsChecked = Convert.ToBoolean(Common.appSettings.EachRowTrans);
+        }
+
+        private void FirstTransCombox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Common.appSettings.FirstTranslator = CommonFunction.lstTranslator[(string)FirstTransCombox.SelectedValue];
+        }
+
+        private void SecondTransCombox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Common.appSettings.SecondTranslator = CommonFunction.lstTranslator[(string)SecondTransCombox.SelectedValue];
+        }
+        
+        private void EachRowTransCheckBox_Click(object sender, RoutedEventArgs e)
+        {
+            Common.appSettings.EachRowTrans = Convert.ToString(EachRowTransCheckBox.IsChecked);
+        }
+    }
+}
