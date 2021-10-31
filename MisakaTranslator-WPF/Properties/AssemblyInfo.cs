3f39d887d4509e17810d6167e5636508eff79578
谭九鼎 <109224573@qq.com>
2021-10-31 16:47:53 +0800

    更新到2.9

--- MisakaTranslator-WPF/Properties/AssemblyInfo.cs
+++ MisakaTranslator-WPF/Properties/AssemblyInfo.cs
@@ -12,5 +12,5 @@ using System.Windows;
 [assembly : AssemblyCulture("")]
 [assembly : ComVisible(false)]
 [assembly : ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
-[assembly : AssemblyVersion("2.8.0.0")]
-[assembly : AssemblyFileVersion("2.8.0.0")]
+[assembly : AssemblyVersion("2.9.0.0")]
+[assembly : AssemblyFileVersion("2.9.0.0")]
