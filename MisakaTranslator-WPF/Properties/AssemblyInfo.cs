3d8953fa35577938249926e6832fd5eda836fb12
imba-tjd <109224573@qq.com>
2022-02-02 19:22:19 +0800

    2.10.1

--- MisakaTranslator-WPF/Properties/AssemblyInfo.cs
+++ MisakaTranslator-WPF/Properties/AssemblyInfo.cs
@@ -12,5 +12,5 @@ using System.Windows;
 [assembly : AssemblyCulture("")]
 [assembly : ComVisible(false)]
 [assembly : ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
-[assembly : AssemblyVersion("2.10.0.0")]
-[assembly : AssemblyFileVersion("2.10.0.0")]
+[assembly : AssemblyVersion("2.10.1.0")]
+[assembly : AssemblyFileVersion("2.10.1.0")]
