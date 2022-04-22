e17eaf60b402dcc25b2de94b518f6c1b324889e2
Code Melody <lhr626333250@hotmail.com>
2022-04-22 12:10:27 +0800

    WPF主程序项目迁移至.NET SDK

--- MisakaTranslator-WPF/Properties/AssemblyInfo.cs
+++ MisakaTranslator-WPF/Properties/AssemblyInfo.cs
@@ -1,16 +1,10 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-using System.Windows;
+﻿using System.Windows;
 
-[assembly : AssemblyTitle("MisakaTranslator_WPF")]
-[assembly : AssemblyDescription("")]
-[assembly : AssemblyConfiguration("")]
-[assembly : AssemblyCompany("")]
-[assembly : AssemblyProduct("MisakaTranslator_WPF")]
-[assembly : AssemblyCopyright("Copyright © MisakaTranslator")]
-[assembly : AssemblyTrademark("")]
-[assembly : AssemblyCulture("")]
-[assembly : ComVisible(false)]
-[assembly : ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
-[assembly : AssemblyVersion("2.11.0.0")]
-[assembly : AssemblyFileVersion("2.11.0.0")]
+[assembly: ThemeInfo(
+    ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
+                                     //(used if a resource is not found in the page,
+                                     // or application resource dictionaries)
+    ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
+                                              //(used if a resource is not found in the page,
+                                              // app, or any theme specific resource dictionaries)
+)]
