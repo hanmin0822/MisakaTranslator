73ce549d83366d25173d8edc8bef2c1a60d192e6
NewBugger <41441631+NewBugger@users.noreply.github.com>
2022-11-24 13:05:28 +0800

    introduce a new tesseract

--- OCRLibrary/TesseractOCR.cs
+++ OCRLibrary/TesseractOCR.cs
@@ -1,41 +1,43 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Tesseract;
+using Tesseract.Pix;
 
 namespace OCRLibrary
 {
     public class TesseractOCR : OCREngine
     {
-        public string srcLangCode;//OCR识别语言 jpn=日语 eng=英语
-        private TesseractEngine TessOCR;
+        private string srcLangCode;  //OCR识别语言 jpn=日语 eng=英语
+        private Engine engine;
 
-        public override Task<string> OCRProcessAsync(Bitmap img)
+        public override async Task<string> OCRProcessAsync(Bitmap img)
         {
             try
             {
-                using (var page = TessOCR.Process(img))
-                {
-                    string res = page.GetText();
-                    return Task.FromResult(res);
-                }
+                var stream = new MemoryStream();
+                img.Save(stream, Imaging.ImageFormat.Bmp);
+                var pix = Image.LoadFromMemory(stream.GetBuffer(), 0, Convert.ToInt32(stream.Length));
+                var recog = engine.Process(pix);
+                stream.Dispose();
+                return Task.FromResult(recog.Text).Result;
             }
             catch (Exception ex)
             {
                 errorInfo = ex.Message;
-                return Task.FromResult<string>(null);
+                return Task.FromResult(string.Empty).Result;
             }
-            
         }
 
         public override bool OCR_Init(string param1 = "", string param2 = "")
         {
             try
             {
-                TessOCR = new TesseractEngine(Environment.CurrentDirectory + "\\tessdata", srcLangCode, EngineMode.Default);
+                engine = new Engine(Environment.CurrentDirectory + "\\tessdata", srcLangCode);
                 return true;
             }
             catch(Exception ex)
@@ -44,9 +46,17 @@ namespace OCRLibrary
                 return false;
             }
         }
+
         public override void SetOCRSourceLang(string lang)
         {
-            srcLangCode = lang;
+            if (lang == "jpn")
+            {
+                srcLangCode = "jpn";
+            }
+            else if (lang == "eng")
+            {
+                srcLangCode = "eng";
+            }
         }
     }
 }
