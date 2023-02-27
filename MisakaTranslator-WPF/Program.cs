a732a096eaec1597ddbf62a13cbc8a3f168d7b44
AndromedaMelody <57533055+AndromedaMelody@users.noreply.github.com>
2023-02-27 23:01:52 +0800

    拒绝提权后可继续以标准用户令牌运行

---
+++ MisakaTranslator-WPF/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+using Windows.Win32;
+using Windows.Win32.Foundation;
+using Windows.Win32.Security;
+
+namespace MisakaTranslator_WPF
+{
+    static internal class Program
+    {
+        static unsafe bool IsElevated()
+        {
+            if (PInvoke.OpenProcessToken(Process.GetCurrentProcess().SafeHandle, TOKEN_ACCESS_MASK.TOKEN_ALL_ACCESS, out var hToken))
+            {
+                TOKEN_ELEVATION tokenType = new();
+                if (PInvoke.GetTokenInformation(hToken, TOKEN_INFORMATION_CLASS.TokenElevation, &tokenType, (uint)Marshal.SizeOf(tokenType), out var _))
+                {
+                    return (BOOL)(int)tokenType.TokenIsElevated;
+                }
+            }
+            return false;
+        }
+
+        static unsafe bool CanElevate()
+        {
+            if (PInvoke.OpenProcessToken(Process.GetCurrentProcess().SafeHandle, TOKEN_ACCESS_MASK.TOKEN_ALL_ACCESS, out var hToken))
+            {
+                TOKEN_ELEVATION_TYPE tokenType = new();
+                if (PInvoke.GetTokenInformation(hToken, TOKEN_INFORMATION_CLASS.TokenElevationType, &tokenType, sizeof(TOKEN_ELEVATION_TYPE), out var _))
+                {
+                    return tokenType == TOKEN_ELEVATION_TYPE.TokenElevationTypeLimited;
+                }
+            }
+            return false;
+        }
+
+        static bool RunElevate()
+        {
+            ProcessStartInfo elevationInfo = new()
+            {
+                FileName = Process.GetCurrentProcess().ProcessName,
+                UseShellExecute = true,
+                Verb = "runas",
+                WorkingDirectory = Environment.CurrentDirectory
+            };
+            try
+            {
+                Process.Start(elevationInfo);
+                Environment.Exit(0);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        [STAThread]
+        static void Main()
+        {
+            Environment.CurrentDirectory = Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName);
+            if (CanElevate())
+            {
+                RunElevate();
+            }
+            App.Main();
+        }
+    }
+}
